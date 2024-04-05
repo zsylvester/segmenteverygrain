6d53656ea9c7ca67fdcc83b0aefa30f4ad144b71
Zoltan Sylvester <zoltan.sylvester@gmail.com>
2024-04-05 15:42:26 -0500

    switch to setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+long_description = """\
+'segmenteverygrain' is a Python package that aims to detect grains (or grain-like objects) in images. 
+The goal is to develop an ML model that does a reasonably good job at detecting most of the grains in a photo, 
+so that it will be useful for determining grain size and grain shape, a common task in geomorphology and sedimentary geology.
+"""
+
+setuptools.setup(
+    name="segmenteverygrain",
+    version="0.1.6",
+    author="Zoltan Sylvester",
+    author_email="zoltan.sylvester@beg.utexas.edu",
+    description="a SAM-based model for segmenting grains in images of grains",
+    keywords = 'sedimentology, geomorphology, grain size, segment anything model',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zsylvester/segmenteverygrain",
+    packages=['segmenteverygrain'],
+    install_requires=['numpy','matplotlib',
+        'scipy','pillow','scikit-image','tqdm','opencv-python',
+        'networkx','rasterio','shapely','tensorflow','segment-anything'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
