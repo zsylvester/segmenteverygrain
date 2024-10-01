f87f5a392ed886700d6681405e3a48dd9f196672
Dave Matthews <DjmPhysics@gmail.com>
2024-10-01 11:36:32 -0500

    tweak conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,32 +6,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-from unittest import mock
-
-# Mock open3d because it fails to build in readthedocs
-MOCK_MODULES = ["numpy", "matplotlib", "matplotlib.pyplot", "pandas", "scipy", "skimage", "tqdm", 
-    "rtree", "itertools", "networkx", "rasterio", "rasterio.features", "shapely", "tensorflow",
-    "skimage.measure", "skimage.morphology", "skimage.segmentation", "skimage.feature", "shapely.geometry", "shapely.affinity",
-    "scipy.ndimage", "sklearn.cluster", "tensorflow.keras.models", "tensorflow.keras.layers", 
-    "tensorflow.keras.preprocessing.image", "segment_anything"]
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
-
-# Example of setting return values for specific methods that need to be iterable
-mock_numpy = sys.modules['numpy']
-mock_numpy.array.return_value = mock.Mock()
-mock_numpy.array.return_value.__iter__ = lambda x: iter([])
-
-mock_pandas = sys.modules['pandas']
-mock_pandas.DataFrame.return_value = mock.Mock()
-mock_pandas.DataFrame.return_value.__iter__ = lambda x: iter([])
-
-# Mock common iterable methods for other modules
-for mod_name in MOCK_MODULES:
-    mock_module = sys.modules[mod_name]
-    mock_module.items.return_value = iter([])
-    mock_module.keys.return_value = iter([])
-    mock_module.values.return_value = iter([])
 
 
 # -- Project information -----------------------------------------------------
