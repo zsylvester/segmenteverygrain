8bc8e716a45eda3f481ec22b888cdf7b39f6760e
Zoltan Sylvester <zoltan.sylvester@gmail.com>
2024-10-01 05:09:47 -0500

    add documentation folder

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'segmenteverygrain'
+copyright = '2024, Zoltan Sylvester'
+author = 'Zoltan Sylvester'
+release = '0.1.8'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, '/Users/zoltan/Dropbox/Segmentation/segmenteverygrain/segmenteverygrain/__init__.py')
