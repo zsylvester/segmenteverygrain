2f1b930c53a6bff49ba04614b71267f8dae24816
Dave Matthews <DjmPhysics@gmail.com>
2024-10-31 13:12:32 -0500

    change version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ so that it will be useful for determining grain size and grain shape, a common t
 
 setuptools.setup(
     name="segmenteverygrain",
-    version="0.1.9",
+    version="1.0.0",
     author="Zoltan Sylvester",
     author_email="zoltan.sylvester@beg.utexas.edu",
     description="a SAM-based model for segmenting grains in images of grains",
