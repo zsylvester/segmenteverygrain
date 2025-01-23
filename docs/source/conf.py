79192b0519e8dfd70f5c428b510633ca9e46c03a
Zoltan Sylvester <zoltan.sylvester@gmail.com>
2025-01-23 12:10:06 -0600

    change docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,5 +32,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
